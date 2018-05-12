8cbc7cc469ae74f17ef0ca0aa8db5f8afadad377
Christian <chr_414@hotmail.com>
2018-05-12 12:51:13 +0200

    projectiles cannot collide with eachother

--- web/game_object_components/custom_contact_filter.dart
+++ web/game_object_components/custom_contact_filter.dart
@@ -2,6 +2,7 @@ import 'package:box2d/box2d.dart';
 import '../abstract_game_object.dart';
 import '../controllable_game_object.dart';
 import '../projectile_game_object.dart';
+import '../game_object_type.dart';
 
 class CustomContactFilter extends ContactFilter {
 	@override
@@ -12,6 +13,8 @@ class CustomContactFilter extends ContactFilter {
 		if (gameObj1 is ControllableGameObject && gameObj2 is ProjectileGameObject) {
 			return false;
 		} else if (gameObj1 is ProjectileGameObject && gameObj2 is ControllableGameObject) {
+			return false;
+		} else if (gameObj1.gameObjectType == GameObjectType.PROJECTILE && gameObj2.gameObjectType == GameObjectType.PROJECTILE) {
 			return false;
 		} else {
 			return super.shouldCollide(fixtureA, fixtureB);
